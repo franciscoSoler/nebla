c9efd5b64da9f9856d46617919fe075bea68d388
adrianmdu@gmail.com <adrianmdu@gmail.com@7bc8635f-7fb9-ff4d-a55d-83a11fa4ca6c>
2013-09-27 00:33:51 +0000

    Ej 1

---
+++ trunk/distribuidos/Duran/ej1/src/common.h
@@ -0,0 +1,22 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define TAM_BUFFER 255
+
+#define DIRECTORY "/home/distribuidos/Duran"
+
+#define ID_SEMCONTROL		1
+#define ID_SHMEM		2
+
+typedef struct {
+	bool abierto;
+	int  cantidad;
+	int  total;
+	int  cantidadMaxima;
+} InfoMuseo;
+
+#endif
