8911920bfc01300295ccf5832f817a9cff2a6de9
adrianmdu@gmail.com <adrianmdu@gmail.com@7bc8635f-7fb9-ff4d-a55d-83a11fa4ca6c>
2013-11-03 05:01:10 +0000

    Agrego logs al controlador robot 5 - cinta

--- trunk/distribuidos/EjGrupal/src/ControladorRobot5Cinta.cpp
+++ trunk/distribuidos/EjGrupal/src/ControladorRobot5Cinta.cpp
@@ -16,6 +16,7 @@
 #include "IPCs/IPCAbstractos/MessageQueue/PedidosProduccionMessageQueue.h"
 
 #include "Common.h"
+#include "Logger.h"
 
 void iniciarIPC(IPC::ComunicacionRobot5MessageQueue &colaComunicacionRobot5,
         IPC::PedidosProduccionMessageQueue &colaPedidos, 
@@ -55,7 +56,10 @@ void leerEstadoCintas (CintaTransportadora6 *cintaTransportadora,
 }
 
 int main(int argc, char** argv) {
-
+    
+     char buffer[TAM_BUFFER];
+     Logger::getInstance().setProcessInformation("Controlador Robot 5 - Cinta:");
+   
      /* Se crean e inician todos los ipc necesarios para
       * el funcionamiento del proceso.
       */
@@ -80,11 +84,12 @@ int main(int argc, char** argv) {
                 semaforoBloqueoRobot11);
     }
     catch (Exception const& ex) {
-        char buffer[TAM_BUFFER];
         sprintf (buffer, "Controlador Robot 5 - Cinta: Error: %s\n", ex.what());
-        write (fileno(stderr),buffer, strlen(buffer));
+        Logger::getInstance().logMessage(Logger::ERROR, buffer);
         exit(-1);
     }
+    
+    Logger::getInstance().logMessage(Logger::TRACE, "Iniciando");
     
     EstadoCinta estadoCinta0;
     EstadoCinta estadoCinta1;
@@ -93,10 +98,20 @@ int main(int argc, char** argv) {
 
     while (deboSeguir) {
 
+        Logger::getInstance().logMessage(Logger::TRACE, "Esperando una orden de produccion.");
+        
         /* Recibo una orden de produccion */
         MensajePedidoProduccion mensajePedidoProduccion;
         colaPedidosProduccion.recibirPedidoProduccion(TIPO_PEDIDO_PRODUCCION, &mensajePedidoProduccion);
 
+        sprintf(buffer, "Recibí un pedido de produccion:\n\tOrden de compra: %d Tipo producto: %d Cantidad: %d Diferencia: %d",
+                mensajePedidoProduccion.pedidoProduccion.nroOrdenCompra,
+                mensajePedidoProduccion.pedidoProduccion.tipo,
+                mensajePedidoProduccion.pedidoProduccion.cantidad,
+                mensajePedidoProduccion.pedidoProduccion.diferenciaMinima);
+        
+        Logger::getInstance().logMessage(Logger::TRACE, buffer);
+        
         /* Le envio la orden de producción al robot 5 */
         PedidoRobot5 pedidoRobot5;
         pedidoRobot5.tipo = PEDIDO_PRODUCCION;
@@ -106,16 +121,23 @@ int main(int argc, char** argv) {
         mensajePedidoRobot5.mtype = TIPO_PEDIDO_ROBOT_5;
         mensajePedidoRobot5.pedidoRobot5 = pedidoRobot5;
 
+        Logger::getInstance().logMessage(Logger::TRACE, "Enviando pedido de produccion a la App.");
+        
         /* Le envio el pedido de produccion al robot 5 Aplicacion */
         colaComunicacionRobot5.enviarPedidoRobot5(mensajePedidoRobot5);
 
         bool ultimo = false;
+        
+        Logger::getInstance().logMessage(Logger::TRACE, "Comienzo a depositar productos en la cinta.");
+        
         /* Sigo depositando gabinetes mientras no reciba el ultimo de este pedido */
-        while (!ultimo) {         
+        while (!ultimo) {
             /* Obtengo los estados de ambas cintas para intentar depositar el primer gbainete */
+            Logger::getInstance().logMessage(Logger::TRACE, "Verificando estado de las cintas.");
             leerEstadoCintas(cintasTransportadoras, estadoCinta0, estadoCinta1);
             bool bloqueadas = (estadoCinta0.ocupado && estadoCinta1.ocupado);
             while (bloqueadas) {
+                Logger::getInstance().logMessage(Logger::TRACE, "Ambas cintas estan bloqueadas.");
                 /* Como ambas cintas tienen su primer lugar ocupado, 
                  * aviso que me voy a bloquear y me bloqueo */
                 semaforoAccesoEstadoRobot5.wait();
@@ -132,6 +154,8 @@ int main(int argc, char** argv) {
                 leerEstadoCintas(cintasTransportadoras, estadoCinta0, estadoCinta1);
                 bloqueadas = (estadoCinta0.ocupado && estadoCinta1.ocupado);
             }
+            
+            Logger::getInstance().logMessage(Logger::TRACE, "Se libero un lugar en alguna cinta, envio un pedido por un producto.");
 
             /* A partir de aca, al menos una de las cintas tiene lugar para 
              * depositar un gabinete. 
@@ -148,7 +172,9 @@ int main(int argc, char** argv) {
             MensajeRespuestaGabinete mensajeRespuestaGabinete;
             colaComunicacionRobot5.recibirGabinete(TIPO_MENSAJE_RESPUESTA_GABINETE_ROBOT_5, &mensajeRespuestaGabinete);
             ultimo = mensajeRespuestaGabinete.ultimo;
-                    
+            
+            Logger::getInstance().logMessage(Logger::TRACE, "Recibi un producto para depositar en una cinta.");
+            
             /* Una vez obtenido el gabinete lo deposito en la cinta que convenga.
              * Si ambas estan libres, en aquella que este menos ocupada.
              * Leo nuevamente el estado de ambas cintas para tener una version mas actualizada del mismo */
@@ -157,27 +183,37 @@ int main(int argc, char** argv) {
             int cintaAUtilizar;
             if (! estadoCinta0.ocupado && estadoCinta1.ocupado) {
                 /* Utilizo la cinta 0. */
+                Logger::getInstance().logMessage(Logger::TRACE, "Utilizo la cinta 0, puesto que la 1 esta bloqueada.");
                 cintaAUtilizar = 0;
             }
             else if (estadoCinta0.ocupado && !estadoCinta1.ocupado) {
                 /* Utilizo la cinta 1. */
+                Logger::getInstance().logMessage(Logger::TRACE, "Utilizo la cinta 1, puesto que la 0 esta bloqueada.");
                 cintaAUtilizar = 1;
             }
             else {
                 /* Ambas estan libres, utilizo la que menos gabinetes tenga. */
                 cintaAUtilizar = (estadoCinta0.cantOcupados <= estadoCinta1.cantOcupados) ? 0 : 1;
+                sprintf(buffer,"Ambas cintas estan libres, utilizo %d que es la menos cargada.", cintaAUtilizar);
+                Logger::getInstance().logMessage(Logger::TRACE, buffer);
             }
             
-            /* Una vez obtenida la cinta a utilizar, creo y deposito 
+            /* Una vez obtenida la cinta a utilizar, deposito 
              * el producto en produccion 
              */
+            Logger::getInstance().logMessage(Logger::TRACE, "Deposito el producto en produccion.");
             ProductoEnProduccion productoEnProduccion = mensajeRespuestaGabinete.productoEnProduccion;
             cintasTransportadoras[cintaAUtilizar].depositarProductoEnProduccion(productoEnProduccion);
+
+            sprintf(buffer, "Estado cinta luego del deposito: %s", cintasTransportadoras[cintaAUtilizar].obtenerMensajeEstado().c_str());
+            Logger::getInstance().logMessage(Logger::TRACE, buffer);
             
             /* Verifico si el robot 11 encargado de la cinta utilizada esta 
              * bloqueado 
              */
+            Logger::getInstance().logMessage(Logger::TRACE, "Verifico si el robot 11 correspondiente esta bloqueado.");
             if (cintasTransportadoras[cintaAUtilizar].robot11Bloqueado()) {
+                Logger::getInstance().logMessage(Logger::TRACE, "Robot 11 bloqueado, lo desbloqueo.");
                 cintasTransportadoras[cintaAUtilizar].marcarRobot11Liberado();
                 semaforoBloqueoRobot11.signal(cintaAUtilizar);
             }
