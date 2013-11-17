be068cf6250ba10aa4a043c32f0c30ef0bc4c800
ezequiel.torresfeyuk <ezequiel.torresfeyuk@gmail.com>
2013-11-17 14:54:53 +0000

    * Se limpia el directorio raiz del proyecto

--- distribuidos/EjGrupal/src/definedTypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEFINEDTYPES_H
-#define	DEFINEDTYPES_H
-
-typedef unsigned int uint;
-// Tipo para utilizar en los IDs de cualquier tipo en el proyecto
-// typedef unsigned long int id_t;
-
-#endif	/* DEFINEDTYPES_H */
-
