2ace4202007006903d50d98ec86da9839c59a77f
ezequiel.torresfeyuk@gmail.com <ezequiel.torresfeyuk@gmail.com@7bc8635f-7fb9-ff4d-a55d-83a11fa4ca6c>
2013-12-11 02:12:45 +0000

    * Se crean colas dedicadas por tipo de agente para mensajes para el Broker de Semáforos y SharedMemories

--- trunk/distribuidos/EjGrupal/branches/Iteración_2/src/Comunicaciones/Agentes/LauncherAgentes.cpp
+++ trunk/distribuidos/EjGrupal/branches/Iteración_2/src/Comunicaciones/Agentes/LauncherAgentes.cpp
@@ -40,6 +40,7 @@ void createIPCs() {
     Logger::getInstance().createLog();
     Logger::getInstance().setProcessInformation("LauncherAgentes:");
 
+    // MsgQueues utilizadas para mensajes
     IPC::MsgQueue commColaSalida;
 
     commColaSalida.create(DIRECTORY_COMM, ID_TIPO_CLIENTE);
@@ -77,7 +78,43 @@ void createIPCs() {
 
     commColaSalida.create(DIRECTORY_COMM, ID_TIPO_DESPACHO);
     Logger::logMessage(Logger::COMM, "Cola CanalSalida-Despacho creada");
-    
-    commColaSalida.create(DIRECTORY_SEM, ID_COMM_SEM_ENTRADA);
-    Logger::logMessage(Logger::COMM, "Cola CanalEntrada-Semaforos creada");
+
+    // MsgQueues utilizadas para SharedMemories y Semáforos
+    IPC::MsgQueue commSemaphore;
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_CLIENTE);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-cliente creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_VENDEDOR);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-vendedor creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_AP);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-AlmacenDePiezas creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_AGV);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-AGV creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT5_AGV);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot5AGV creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT5_CINTA);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot5Cinta creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT11);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot11 creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT12);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot12 creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT14);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot14 creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT16_CINTA);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot16Cinta creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_ROBOT16_DESPACHO);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Robot16Despacho creada");
+
+    commSemaphore.create(DIRECTORY_SEM, ID_TIPO_DESPACHO);
+    Logger::logMessage(Logger::COMM, "Cola Semaforo-SharedMemory-Despacho creada");
 }
