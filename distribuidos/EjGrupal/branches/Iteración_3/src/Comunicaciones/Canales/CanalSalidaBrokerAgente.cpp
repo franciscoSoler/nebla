da37f637057f1c7804f225b052b1ba82e0ed774f
ezequiel.torresfeyuk <ezequiel.torresfeyuk@gmail.com>
2013-12-15 15:25:01 +0000

    * Fix en CanalSalidaBrokerAgente

--- distribuidos/EjGrupal/branches/Iteración_3/src/Comunicaciones/Canales/CanalSalidaBrokerAgente.cpp
+++ distribuidos/EjGrupal/branches/Iteración_3/src/Comunicaciones/Canales/CanalSalidaBrokerAgente.cpp
@@ -15,18 +15,25 @@
 #include "middlewareCommon.h"
 
 int main(int argc, char* argv[]) {
-    Logger::setProcessInformation("CanalSalidaBrokerAgente:");
     char buffer[TAM_BUFFER];
     char bufferSocket[TAM_BUFFER];
     char bufferMsgQueue[MSG_BROKER_SIZE];
-    CommunicationsUtil util;
+    ArgumentParser argParser(argc, argv);
     int idSd = 0;
     long idAgente = 0;
+    int brokerNumber = 0;
     int idTipoAgente;
 
-    if ( util.parseArgs(argc, argv, idSd) == -1 ) {
+    if ( argParser.parseArgument(1, idSd) == -1 ) {
         exit(-1);
     }
+
+    if ( argParser.parseArgument(2, brokerNumber) == -1 ) {
+        exit(-1);
+    }
+
+    sprintf(buffer, "CanalEntradaBrokerAgente N°%d", brokerNumber);
+    Logger::setProcessInformation( buffer );
 
     SocketStream socketAgente(idSd);
     Logger::logMessage(Logger::COMM, "Canal de Salida conectado");
@@ -57,7 +64,7 @@ int main(int argc, char* argv[]) {
             colaBroker.recv(idAgente, bufferMsgQueue, MSG_BROKER_SIZE);
             memcpy(&mensaje, bufferMsgQueue, sizeof(MsgCanalSalidaBroker));
             
-            sprintf(buffer, "Recibe mensaje de Broker, con tamaño %lud", sizeof(mensaje));
+            sprintf(buffer, "Recibe mensaje de Broker, con tamaño %d", sizeof(mensaje));
             Logger::logMessage(Logger::COMM, buffer);
             
             sprintf(buffer, "parametros: mtype recibido (idAgente): %ld", 
