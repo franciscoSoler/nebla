f14f4e09f3a6208d93f39b7538689aa7a8016100
ezequiel.torresfeyuk <ezequiel.torresfeyuk@gmail.com>
2013-12-25 18:51:44 +0000

    intentando cambiar que el idControladorRobot5Cinta tenga id = 2

--- distribuidos/EjGrupal/branches/Iteración_3/src/API/Robot5/ControladorRobot5Cinta.cpp
+++ distribuidos/EjGrupal/branches/Iteración_3/src/API/Robot5/ControladorRobot5Cinta.cpp
@@ -11,9 +11,9 @@
 #include "../../Parser/Parser.h"
 
 ControladorRobot5Cinta::ControladorRobot5Cinta() :
-        colaPedidosProduccion("PedidosProduccionMessageQueue", 1, ID_TIPO_ROBOT5_CINTA, ID_TIPO_AP),
+        colaPedidosProduccion("PedidosProduccionMessageQueue", 2, ID_TIPO_ROBOT5_CINTA, ID_TIPO_AP),
         estadoRobot5("EstadoRobot5SharedMemory"),        
-        semaforoAccesoEstadoRobot5("AccesoEstadoRobot5", 1, ID_TIPO_ROBOT5_CINTA),
+        semaforoAccesoEstadoRobot5("AccesoEstadoRobot5", 2, ID_TIPO_ROBOT5_CINTA),
         semaforoBloqueoRobot5("BloqueoRobot5", 1, ID_TIPO_ROBOT5_CINTA),
         semaforoBloqueoRobot11("BloqueoRobot11", 1, ID_TIPO_ROBOT5_CINTA, ID_TIPO_ROBOT11),
         semaforoApiRobot5("ApiRobot5")
