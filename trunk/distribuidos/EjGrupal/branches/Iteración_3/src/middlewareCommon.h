3baff8cb87bccdea85e9854df9c50362408f38aa
ezequiel.torresfeyuk@gmail.com <ezequiel.torresfeyuk@gmail.com@7bc8635f-7fb9-ff4d-a55d-83a11fa4ca6c>
2013-12-26 00:28:48 +0000

    * Modifico número de puertos. Les sumo un offset de 50k. (Parece ser que muchos   bugs de los que estamos teniendo, tienen que ver con esto. Habría que averiguar   una forma de poder conocer si un puerto está siendo usado al momento de crear   nuestros servicios) * Se modifica la variable que indica el tamaño de las SharedMemorys de InfoAgentes.   Estaba en 10, número muy bajo para correr esto con más de 10 clientes.

--- trunk/distribuidos/EjGrupal/branches/Iteración_3/src/middlewareCommon.h
+++ trunk/distribuidos/EjGrupal/branches/Iteración_3/src/middlewareCommon.h
@@ -72,7 +72,7 @@ typedef enum {
 
 // Cantidad de agentes por tipo. Se utiliza en la shMem que posee
 // la información de que agente está conectado en cada Broker
-#define MAX_AMOUNT_AGENTS       10
+#define MAX_AMOUNT_AGENTS       50
 // Cantidad de Agentes en el sistema. Equivale al tamaño del
 // struct TipoAgente - 1 (por el tipo ID_TIPO_VACIO)
 #define AMOUNT_AGENTS           12
